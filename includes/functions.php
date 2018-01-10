89fa81aa0da9b51dee5511ffc3d821e132b01763
connornclark <connor.n.clark@gmail.com>
2018-01-10 10:29:11 -0500

    started on AJAX

---
+++ includes/functions.php
@@ -0,0 +1,42 @@
+<?php
+  $user = 'root';
+  $pass = 'root'; // this should be a blank host for windows machines
+  $host = 'localhost';
+  $db = 'portfoliosite'; // whatever you called your dataBase
+
+  $conn = mysqli_connect($host, $user, $pass, $db);
+  mysqli_set_charset($conn, 'utf8');
+
+  if (!$conn) {
+    echo "Something Broke";
+    exit;
+  }
+
+  //echo "Connected!";
+
+
+  $myQuery = "SELECT * FROM mainmodel";
+  $result = mysqli_query($conn, $myQuery);
+
+  $rows = array();
+
+  while($row = mysqli_fetch_assoc($result)) {
+    $rows[] = $row;
+  }
+
+  //var_dump($rows);
+  //echo json_encode($rows);
+
+  //get a single row using a query parameter
+  if (isset($_GET['carModel'])) { //see if there's  parameter called carModel
+    $car = $_GET['carModel']; //whatever comes after the ?
+
+  $myQuery = "SELECT * FROM mainmodel WHERE model = '$car'";
+  $result = mysqli_query($conn, $myQuery);
+
+  $row = mysqli_fetch_assoc($result);
+
+  echo json_encode($row);
+  }
+
+?>
--- js/app.js
+++ js/app.js
@@ -56,3 +56,71 @@ function workAnimationOut() {
 
 /*Reset WTV gif everytime the page is loaded*/
 img.src = "wtvLogo.gif"+"?a="+Math.random();
+
+
+/*
+-----------------------------------------
+AJAX
+-----------------------------------------
+*/
+
+(function () {
+  // start with retrieving the elements from the page, and then adding event handling. then write the logic. refer to the seasons example / homework
+   var portfolioImages = document.querySelectorAll('.data-ref');
+
+   const httpRequest = new XMLHttpRequest();
+
+   function getCarData() {
+
+        const url = './includes/functions.php?carModel=' +this.id;
+
+        //the fetch API uses new JavaScript Promise API
+        fetch(url) //do an AJAX call with fetch
+          .then((resp) => resp.json()) //convert to json
+          .then(({ modelName, pricing, modelDetails, model }) => {
+            let carModel = document.querySelector('.modelName').textContent = modelName;
+            let price = document.querySelector('.priceInfo').innerHTML = pricing;
+            let desc = document.querySelector('.modelDetails').textContent = modelDetails;
+
+             //change opacity
+             carImages.forEach(function(car, index) {
+                 car.classList.add('nonActive');
+             });
+
+             document.querySelector(`#${model}`).classList.remove('nonActive');
+          })
+          .catch(function(error) {
+            //catch any error and report it to the console
+            console.log(error);
+          });
+        }
+
+   function processResult(data) {
+     const { modelName, pricing, modelDetails } = data;
+     /*let model = document.querySelector('.modelName').textContent = modelName;
+     let price = document.querySelector('.priceInfo').innerHTML = pricing;
+     let desc = document.querySelector('.modelDetails').textContent = modelDetails;
+
+     let objectIndex = carData [this.id];
+
+      //change opacity
+      carImages.forEach(function(image, index){
+          image.classList.add("nonActive");
+      });
+
+      document.querySelector(`#${data.model}`).classList.remove('nonActive');
+      //remove opacity when clicked
+      //this.classList.remove('nonActive');*/
+   }
+
+
+   carImages.forEach(function(image, index){
+     image.addEventListener('click', getCarData, false);
+   });
+
+   //Preload low opacity
+   getCarData.call(document.querySelector('#F55'));
+
+
+
+})();
