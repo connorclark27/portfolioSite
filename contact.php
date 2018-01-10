875b934cc13016ec39ca80f6dc86b12672e338a8
connornclark <connor.n.clark@gmail.com>
2018-01-09 19:03:03 -0500

    contact & thanks page complete

--- contact.php
+++ contact.php
@@ -59,7 +59,7 @@
   </div>-->
 
 <section class="contactParagraph row">
-  <section class="small-12 medium-6 large-8 columns">
+  <section class="small-12 medium-12 large-8 columns">
     <section class="contactText">
     <p id="contactParagraph">
       Have a cool project in mind? I'm available for freelance work - so fill out the contact form with a few details and I’ll get back to you as soon as I can!
@@ -68,23 +68,30 @@
     <p class="socialMedia">
       Follow me on Instagram for pictures of my dog, or connect with me on LinkedIn!
     </p>
+
+    <div id="socialIconCont">
+    	<a href="https://www.instagram.com/connor.clark/" target="_blank"><img src="images/instagram.svg" class="socialIcons"></a>
+    	<a href="https://www.linkedin.com/in/connor-clark-5a80ab148/" target="_blank"><img src="images/linkedIn.svg" class="socialIcons"></a>
+    </div>
     </section>
   </section>
 
 
-  <div class="small-12 medium-6 large-4 columns">
-  <form action="contact.php" method="post" class="contactForm"> 
-    <!--Make sure to give each input a name attribute(name="")-->
-       <label>Name:</label>
-       		<input id="nameInput" name="name" type="text" size="21" length="30"/>
-       <label>Email:</label>
-       		<input id="emailInput" name="email" type="text" size="21" maxlength="30" />
-       		<!--Catch Bots-->
-       <label for="street" class="streetHide">Street: </label><input name="street" type="text" size="21" maxlength="30" class="streetHide"/>
-       <label for="message">Message:</label>
-       		<textarea id="messageInput" name="message"></textarea>
-       <input name="submit" type="submit" value="Send" />
-    </form>
+  <div class="small-12 medium-12 large-4 columns">
+	  <div class="formCenter">
+	  <form action="contact.php" method="post" class="contactForm"> 
+	    <!--Make sure to give each input a name attribute(name="")-->
+	       <label>Name:</label>
+	       		<input id="nameInput" name="name" type="text" size="21" length="30"/>
+	       <label>Email:</label>
+	       		<input id="emailInput" name="email" type="text" size="21" maxlength="30" />
+	       		<!--Catch Bots-->
+	       <label for="street" class="streetHide">Street: </label><input name="street" type="text" size="21" maxlength="30" class="streetHide"/>
+	       <label for="message">Message:</label>
+	       		<textarea id="messageInput" name="message"></textarea>
+	       <input id="sendButton" name="submit" type="submit" value="Send" />
+	    </form>
+	    </div>
     </div>
 
     <script src="js/vendor/jquery.min.js"></script>
