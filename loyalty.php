cac16255f3a96b60986494b16e8e9ff7078461e3
connornclark <connor.n.clark@gmail.com>
2018-01-10 00:41:07 -0500

    loyalty page roughed in

---
+++ loyalty.php
@@ -0,0 +1,68 @@
+<?php
+?>
+
+
+<!doctype html>
+<html class="no-js" lang="en">
+  <head>
+    <meta charset="utf-8" />
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <meta name="viewport" content="width=device-width">
+    <title>Connor Clark | vivaNext</title>
+    <link rel="stylesheet" href="css/foundation.css" />
+    <link rel="stylesheet" href="css/app.css" />
+    <link rel="stylesheet" href="https://use.typekit.net/gpa5ynz.css">
+  </head>
+
+  <body id="thanksBody">
+  <div id="sideNavigation" class="sidenav">
+    <a href="index.php" id="page1">HOME</a>
+    <a href="work.php" id="page2">WORK</a>
+    <a href="contact.php" id="page3">CONTACT</a>
+  </div>
+
+  <div id="hamburgMove">
+    <span onclick="openNav()">
+      <div id="nav-iconBlack3" class="hamburgerIcon">
+          <span></span>
+          <span></span>
+          <span></span>
+          <span></span>
+      </div>
+    </span>
+  </div>
+
+  <header>
+    <img src="images/logo_dark.svg" class="logo"/>
+  </header>
+
+  <!--<div id="workHeader" class="homeAnimation1">
+    <h1 id="workHeaderText">WORK</h1>
+  </div>-->
+
+  <section class="row">
+      <section class="small-12 medium-12 large-12 columns">
+          <img id="loyaltyLogo" src="images/loyaltyLogo.svg">
+      </section>
+  </section>
+  <section class="row">
+    <div class="small-12 medium-12 large-12 caseStudySectionCenter columns">
+      <p class="caseParagraphCenter">
+        In 2017, I worked with the Global Development division of LoyaltyOne as a graphic design consultant. My task was to revamp their pitch presentation slide decks used to gain coalition loyalty program partners. I redesigned the previous slides using interactive animations to communicate complex business models in a clear way.  
+      </p>
+    </div>
+  </section>
+
+    <section class="caseStudySection row">
+      <div class="small-12 medium-12 large-12 columns">
+        <a href="work.php"><div id="backToHome">BACK TO WORK</div></a>
+      </div>
+    </section>
+
+    <script src="js/vendor/jquery.min.js"></script>
+    <script src="js/vendor/what-input.min.js"></script>
+    <script src="js/foundation.min.js"></script>
+
+    <script src="js/app.js"></script>
+  </body>
+</html>
